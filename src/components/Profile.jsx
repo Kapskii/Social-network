c052e90150ffc62187abb0f64b54d6d4233ee169
Kapskii <kapskij20@gmail.com>
2023-09-05 12:34:59 +0300

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+    <img className='car' src='https://www.volvocars.com/images/v/-/media/market-assets/us/applications/dotcom/homepage/2560x1080.jpg?h=1080&iar=0&w=2560'/>
+    </div>
+    <div>
+      <img src=''/>
+    </div>
+    </div>
+}
+
+export default Profile;
