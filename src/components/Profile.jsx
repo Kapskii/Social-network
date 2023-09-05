2d442857afb4e2506e6eb1bedf4c42ca80d4ad8e
Kapskii <kapskij20@gmail.com>
2023-09-05 14:01:35 +0300

    module_css

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={s.content}>
     <div>
-    <img className='car' src='https://www.volvocars.com/images/v/-/media/market-assets/us/applications/dotcom/homepage/2560x1080.jpg?h=1080&iar=0&w=2560'/>
+    <img className={s.car} src='https://www.volvocars.com/images/v/-/media/market-assets/us/applications/dotcom/homepage/2560x1080.jpg?h=1080&iar=0&w=2560'/>
     </div>
     <div>
       <img src=''/>
