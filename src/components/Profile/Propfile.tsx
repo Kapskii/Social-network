9818e998acb7d60219e6db89a7388b638348ffb5
Kapskii <kapskij20@gmail.com>
2023-11-03 19:53:04 +0300

    test

--- src/components/Profile/Propfile.tsx
+++ src/components/Profile/Propfile.tsx
@@ -9,6 +9,7 @@ export const Propfile = () => {
                 <div className={s.imageWrapper}>
                     <img className={s.img} src="https://images.unsplash.com/photo-1583668928307-a9c4fc140c48?auto=format&fit=crop&q=80&w=1631&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="desert" />
                 </div>
+                
                 <div className={s.profileWrapper}>
                     <div className={s.profileInfo}>
                         <img src="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg" alt="avatar" />
