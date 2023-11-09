c7b52885170117137116d5cd27d875e9673a33b8
Kapskii <kapskij20@gmail.com>
2023-11-09 12:59:21 +0300

    fix: fix bug

--- src/index.tsx
+++ src/index.tsx
@@ -10,30 +10,30 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-// const postDate = [
-//   {id: '1', post: 'Hello World'},
-//   {id: '2', post: 'My name Slava Kapski'},
-//   {id: '3', post: 'I like React'},
-//   {id: '5', post: 'Good job!'},
-// ]
+const postDate = [
+  {id: '1', post: 'Hello World'},
+  {id: '2', post: 'My name Slava Kapski'},
+  {id: '3', post: 'I like React'},
+  {id: '5', post: 'Good job!'},
+]
 
-// const dialogsDate = [
-//   { id: '1', name: 'Slava' },
-//   { id: '2', name: 'Olya' },
-//   { id: '3', name: 'Vadim' },
-// ]
+const dialogsDate = [
+  { id: '1', name: 'Slava' },
+  { id: '2', name: 'Olya' },
+  { id: '3', name: 'Vadim' },
+]
 
-// const messageDate = [
-//   { id: '1', message: 'Hi' },
-//   { id: '2', message: 'How are you?' },
-//   { id: '3', message: 'Hello' },
-// ]
+const messageDate = [
+  { id: '1', message: 'Hi' },
+  { id: '2', message: 'How are you?' },
+  { id: '3', message: 'Hello' },
+]
 
-// post={postDate} dialogs={dialogsDate} message={messageDate}
+// appState={state}
 
 root.render(
   <BrowserRouter>
-    <App appState={state}  />
+    <App post={postDate} dialogs={dialogsDate} message={messageDate} />
   </BrowserRouter>
 );
 
